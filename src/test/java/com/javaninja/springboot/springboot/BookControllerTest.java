861c75ef4983e8112811983c2b19ca95ff461020
Norris Shelton <Norris.Shelton@cdinteractive.com>
2018-11-03 12:45:34 -0400

    Slight tweak to test class annotations.

--- src/test/java/com/javaninja/springboot/springboot/BookControllerTest.java
+++ src/test/java/com/javaninja/springboot/springboot/BookControllerTest.java
@@ -7,7 +7,6 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
@@ -31,7 +30,8 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 
 @RunWith(SpringRunner.class)
-@SpringBootTest(classes = {Application.class}, webEnvironment = WebEnvironment.DEFINED_PORT)
+@SpringBootTest
+//@SpringBootTest(classes = {Application.class}, webEnvironment = WebEnvironment.DEFINED_PORT)
 public class BookControllerTest {
 
     @Autowired
